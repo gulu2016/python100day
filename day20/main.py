cb5793d94cf5b2fc8fcc0182becb041d939b61c0
gulu2016 <2412165411@qq.com>
2023-06-13 08:53:57 +0800

    day20

--- day20/main.py
+++ day20/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle,Screen
 import time
+from snake import Snake
 
 screen = Screen()
 # s183-1-1-1 创建屏幕，设置背景和标题
@@ -9,17 +10,27 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 # s183-1-1-2 设置snake的初始位置
-starting_positions = [(0,0),(-20,0),(-40,0)]
+# starting_positions = [(0,0),(-20,0),(-40,0)]
 
 segments = []
 
+# s185-1-2-1 创建snake对象
+snake = Snake()
+
+# s186-1-1-1 监听屏幕，对应按键调用对应的函数
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
 # s183-1-1-3 三个位置创建方块，定义Turtle的形状，颜色和位置
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+# for position in starting_positions:
+#     new_segment = Turtle("square")
+#     new_segment.color("white")
+#     new_segment.penup()
+#     new_segment.goto(position)
+#     segments.append(new_segment)
 
 game_is_on = True
 while game_is_on:
@@ -27,12 +38,15 @@ while game_is_on:
     screen.update()
     # s184-1-1-2 延时
     time.sleep(0.1)
-    for seg_num in range(len(segments) -1, 0, -1):
-        # s184-1-1-3 更新turtle位置，后一个覆盖前一个位置，第一个会走到新位置
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x,new_y)
-    segments[0].forward(20)
+    # for seg_num in range(len(segments) -1, 0, -1):
+    #     # s184-1-1-3 更新turtle位置，后一个覆盖前一个位置，第一个会走到新位置
+    #     new_x = segments[seg_num-1].xcor()
+    #     new_y = segments[seg_num-1].ycor()
+    #     segments[seg_num].goto(new_x,new_y)
+    # segments[0].forward(20)
+
+    # s185-1-2-2 调用snake的函数
+    snake.move()
 
 
 screen.exitonclick()
