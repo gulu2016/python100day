2ad37c1d74d09d6b46ac60d5dc3bd1c3c9a87efb
gulu2016 <2412165411@qq.com>
2023-07-15 10:15:12 +0800

    day34

--- day34/main.py
+++ day34/main.py
@@ -1,4 +1,5 @@
 import requests
+import html
 
 parameters = {
     "amount":10,
@@ -12,5 +13,10 @@ response.raise_for_status()
 
 # s303-1-1-2 parse the response and get question context
 data = response.json()
-print(data["results"])
+# s304-1-1-1 get question content which contains html special char
+question_content = data["results"][0]["question"]
+print(question_content)
+# s304-1-1-2 using html.unescape func,convert content to normal
+q_text = html.unescape(question_content)
+print(q_text)
 
